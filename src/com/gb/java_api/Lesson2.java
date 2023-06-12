a747cafc77053e57119f3bb36b10fe6482c50427
dimitri_lda <lukashuk.stifler@mail.ru>
2023-06-12 22:05:10 +0200

    API Lesson 2 - Tasks 3

--- src/com/gb/java_api/Lesson2.java
+++ src/com/gb/java_api/Lesson2.java
@@ -4,24 +4,28 @@ import java.io.*;
 
 /*
  Второй семинар:
- Задание 1: Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
+ Задание 1: Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса,
+ используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
  Если значение null, то параметр не должен попадать в запрос.
  Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
 
  Задание 2 (дополнительное): Дана json-строка (можно сохранить в файл и читать из файла)
  [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
- Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
+ Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида:
+ Студент [фамилия] получил [оценка] по предмету [предмет].
  Пример вывода:
  Студент Иванов получил 5 по предмету Математика.
  Студент Петрова получил 4 по предмету Информатика.
  Студент Краснов получил 5 по предмету Физика.
 
- Задание 3 (дополнительное): Сравнить время выполнения замены символа "а" на "А" любой строки содержащей >1000 символов средствами String и StringBuilder.
+ Задание 3 (дополнительное): Сравнить время выполнения замены символа "а" на "А" любой строки содержащей >1000 символов
+ средствами String и StringBuilder.
  */
 public class Lesson2 {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     private static void task1() {
@@ -60,6 +64,33 @@ public class Lesson2 {
                     jsonValuesArray[2]
             );
         }
+    }
+
+    private static void task3() {
+        String largeString = generateLargeString();
+
+        long startTimeString = System.nanoTime();
+        String replacedWithString = replaceWithString(largeString);
+        long endTimeString = System.nanoTime();
+        long durationString = endTimeString - startTimeString;
+
+        long startTimeStringBuilder = System.nanoTime();
+        String replacedWithStringBuilder = replaceWithStringBuilder(largeString);
+        long endTimeStringBuilder = System.nanoTime();
+        long durationStringBuilder = endTimeStringBuilder - startTimeStringBuilder;
+
+        if (durationString > durationStringBuilder) {
+            System.out.println("StringBuilder быстрее");
+        } else if (durationString < durationStringBuilder) {
+            System.out.println("String быстрее");
+        } else {
+            System.out.println("Результаты равны");
+        }
+
+        System.out.println(
+                "Замена с использованием String заняла: " + durationString + " наносекунд, a co StringBuilder: "
+                        + durationStringBuilder + " наносекунд"
+        );
     }
 
     private static String readFromInputStream(String file) {
@@ -111,5 +142,28 @@ public class Lesson2 {
         }
 
         return jsonValues2DemArray;
+    }
+
+    private static String generateLargeString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 1000; i++) {
+            stringBuilder.append("a");
+            stringBuilder.append("f");
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String replaceWithString(String input) {
+        return input.replace("a", "А");
+    }
+
+    private static String replaceWithStringBuilder(String input) {
+        StringBuilder stringBuilder = new StringBuilder(input);
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (stringBuilder.charAt(i) == 'a') {
+                stringBuilder.setCharAt(i, 'А');
+            }
+        }
+        return stringBuilder.toString();
     }
 }
