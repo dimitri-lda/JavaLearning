dd1bb06b9f93b98540bc14b67e8e9be4b20e0055
dimitri_lda <lukashuk.stifler@mail.ru>
2023-06-14 20:39:34 +0200

    API Lesson 2 - imports fix

--- src/com/gb/java_api/Lesson2.java
+++ src/com/gb/java_api/Lesson2.java
@@ -1,6 +1,11 @@
 package com.gb.java_api;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 /*
  Второй семинар:
