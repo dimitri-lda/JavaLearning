805b704121a8157ef930b12232ca69ebd04ab8b7
dimitri_lda <lukashuk.stifler@mail.ru>
2023-06-18 16:58:03 +0200

    API Lesson 4 - task 4-5

--- src/com/gb/java_api/Lesson4.java
+++ src/com/gb/java_api/Lesson4.java
@@ -18,6 +18,9 @@ public class Lesson4 {
 
         sortByAge(users);
         printUsers(users, "отсортированных по возрасту");
+
+        // Задание 4 и 5
+        indexSorting();
     }
 
     private static void sortByAge(ArrayList<String[]> users) {
@@ -47,6 +50,61 @@ public class Lesson4 {
         for (String[] user : users) {
             System.out.println(
                     user[0] + " " + user[1].charAt(0) + "." + user[2].charAt(0) + ". " + user[3] + " " + user[4]
+            );
+        }
+    }
+
+    private static void indexSorting() {
+        String message = "Введите Фамилию, Иия, Отчество, Возраст и Пол (м,ж) пользователя через пробел. " +
+                "Для выхода введите 0: ";
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> indexes = new ArrayList<>();
+        ArrayList<String> surnames = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> patronymicNames = new ArrayList<>();
+        ArrayList<Integer> ages = new ArrayList<>();
+        ArrayList<Boolean> gender = new ArrayList<>();
+        System.out.println(message);
+        while (!scanner.hasNextInt() || scanner.nextInt() != 0) {
+            String[] users = scanner.nextLine().split(" ");
+            indexes.add(indexes.size());
+            surnames.add(users[0]);
+            names.add(users[1]);
+            patronymicNames.add(users[2]);
+            ages.add(Integer.parseInt(users[3]));
+            gender.add(users[4].toLowerCase().equals("м"));
+            System.out.println(message);
+        }
+
+        indexes.sort(new Comparator<>() {
+            @Override
+            public int compare(Integer i1, Integer i2) {
+                return ages.get(i1).compareTo(ages.get(i2));
+            }
+        });
+
+        System.out.println("\nСписок пользователей отсортированных по возрасту с использованием индексов:");
+        for (int index : indexes) {
+            System.out.println(
+                    surnames.get(index) + " " + names.get(index).charAt(0) + "." + patronymicNames.get(index).charAt(0)
+                            + ". " + ages.get(index) + " " + (gender.get(index) ? "м" : "ж")
+            );
+        }
+
+        indexes.sort(new Comparator<>() {
+            @Override
+            public int compare(Integer i1, Integer i2) {
+                return ages.get(i1) != ages.get(i2)
+                        ? ages.get(i1) - ages.get(i2)
+                        : gender.get(i1).compareTo(gender.get(i2));
+            }
+        });
+
+        System.out.println("\nСписок пользователей отсортированных по возрасту и полу с использованием индексов:");
+        for (int index : indexes) {
+            System.out.println(
+                    surnames.get(index) + " " + names.get(index).charAt(0) + "." + patronymicNames.get(index).charAt(0)
+                            + ". " + ages.get(index) + " " + (gender.get(index) ? "м" : "ж")
             );
         }
     }
