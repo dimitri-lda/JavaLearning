996c2777cc2fe64ce4584e3e669dd3427fadb820
dimitri_lda <lukashuk.stifler@mail.ru>
2023-06-20 13:20:54 +0200

    API Lesson 5 Seminar

--- src/com/gb/java_api/Lesson5sem.java
+++ src/com/gb/java_api/Lesson5sem.java
@@ -2,9 +2,57 @@ package com.gb.java_api;
 
 import java.util.*;
 
+enum Male {
+    John,
+    Dmitrii,
+    Evgenii,
+    Michale,
+    Esteban,
+    Sharle,
+    Fernando,
+    Max,
+    Stan,
+    Niko
+}
+enum Female {
+    Elen,
+    lisa,
+    Monica,
+    Rachel,
+    Anna,
+    Natali,
+    Kira,
+    Nadiya,
+    Maria,
+    Sara
+}
 
 public class Lesson5sem {
     public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    private static void task2() {
+        Map<Integer, String> maleNames = new HashMap<>();
+        Map<Integer, String> femaleNames = new HashMap<>();
+
+        for (var male: Male.values()) {
+            maleNames.put(new Random().nextInt(10), male.toString());
+        }
+        for (var it: maleNames.entrySet()) System.out.println(it.getKey() + ": " + it.getValue());
+        for (var female: Female.values()) {
+            femaleNames.put(new Random().nextInt(10), female.toString());
+        }
+        for (var it: femaleNames.entrySet()) System.out.println(it.getKey() + ": " + it.getValue());
+        for (var maleName: maleNames.entrySet()) {
+            if (femaleNames.containsKey(maleName.getKey())) {
+                System.out.println(maleName.getKey() + ": " + maleName.getValue() + " " + femaleNames.get(maleName.getKey()));
+            }
+        }
+    }
+
+    private static void task1() {
         Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
         for (int i = 0; i < 10; i++) {
             mp.put(i, new Random().nextInt(0, 100));
