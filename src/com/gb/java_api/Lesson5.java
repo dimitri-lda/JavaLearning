905790b60cec93c5ea1f9ed7bed068fc5fa83095
dimitri_lda <lukashuk.stifler@mail.ru>
2023-06-25 23:17:08 +0200

    API Lesson 5

---
+++ src/com/gb/java_api/Lesson5.java
@@ -0,0 +1,43 @@
+package com.gb.java_api;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+ Реализуйте структуру телефонной книги с помощью HashMap.
+ Программа также должна учитывать, что во входной структуре будут повторяющиеся имена с разными телефонами,
+ их необходимо считать, как одного человека с разными телефонами. Вывод должен быть отсортирован по убыванию числа телефонов.
+ */
+public class Lesson5 {
+    public static void main(String[] args) {
+        String message = "Введите Имя и номер телефона пользователя через пробел. " +
+                "Для выхода введите 0: ";
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, ArrayList<Integer>> phonebook = new HashMap<>();
+        System.out.println(message);
+        while (!scanner.hasNextInt() || scanner.nextInt() != 0) {
+            String[] user = scanner.nextLine().split(" ");
+            String name = user[0];
+            Integer phone = Integer.parseInt(user[1]);
+
+            if (phonebook.containsKey(name)) {
+                phonebook.get(name).add(phone);
+            } else {
+                ArrayList<Integer> phones = new ArrayList<>();
+                phones.add(phone);
+                phonebook.put(name, phones);
+            }
+            System.out.println(message);
+        }
+        System.out.println(phonebook);
+
+        ArrayList<String> names = new ArrayList<>(phonebook.keySet());
+        names.sort((o1, o2) -> phonebook.get(o2).size() - phonebook.get(o1).size());
+
+        System.out.println("\nСписок пользователей отсортированных по количеству телефонов:");
+        for (String nameIndex : names) {
+            System.out.println(nameIndex + ": " + phonebook.get(nameIndex));
+        }
+    }
+}
