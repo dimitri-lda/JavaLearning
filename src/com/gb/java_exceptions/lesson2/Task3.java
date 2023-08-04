5c957299d1c534758816b597602ffe292c875c1c
dimitri_lda <lukashuk.stifler@mail.ru>
2023-08-04 16:45:49 +0200

    Java Exceptions - Lesson 2, Task 3

---
+++ src/com/gb/java_exceptions/lesson2/Task3.java
@@ -0,0 +1,50 @@
+package com.gb.java_exceptions.lesson2;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Дан следующий код, исправьте его там, где требуется.
+ */
+public class Task3 {
+//    public static void main(String[] args) throws Exception {
+//        try {
+//            int a = 90;
+//            int b = 3;
+//            System.out.println(a / b);
+//            printSum(23, 234);
+//            int[] abc = { 1, 2 };
+//            abc[3] = 9;
+//        } catch (Throwable ex) {
+//            System.out.println("Что-то пошло не так...");
+//        } catch (NullPointerException ex) {
+//            System.out.println("Указатель не может указывать на null!");
+//        } catch (IndexOutOfBoundsException ex) {
+//            System.out.println("Массив выходит за пределы своего размера!");
+//        }
+//    }
+//    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+//        System.out.println(a + b);
+//    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (FileNotFoundException ex) {
+            System.out.println("Файл не найден!");
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+}
